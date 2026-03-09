2c92f3d77b12c1a7f3dbb791491e311f38ee5825
ArRahmaan17 <ardrah17@gmail.com>
2026-03-09 18:12:57 +0700

    preview image open graph

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -4,8 +4,11 @@
 <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}" />
 <meta name="apple-mobile-web-app-title" content="{{ $app_name ?? 'Todos' }}" />
 <link rel="manifest" href="{{ asset('site.webmanifest') }}" />
-<meta property="og:title" content="{{ $app_name ?? 'Todos' }}">
-<meta property="og:description" content="Simple todo app built with Laravel">
-<meta property="og:image" content="{{ asset('preview.png') }}">
-<meta property="og:url" content="{{ url()->current() }}">
+<meta property="og:title" content="Todo - Secure To-Do App">
+<meta property="og:description" content="Simple todo app built with Laravel with Sanctum authentication">
+<meta property="og:image" content="https://todos.rahmaanms.my.id/preview.png">
+<meta property="og:url" content="https://todos.rahmaanms.my.id">
 <meta property="og:type" content="website">
+
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:image" content="https://todos.rahmaanms.my.id/preview.png">
