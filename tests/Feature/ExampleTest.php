fe60ad4497fb94429565ddb4f27a1dd5c6432cd2
ArRahmaan17 <ardrah17@gmail.com>
2026-03-25 02:18:25 +0700

    feat: test case

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -11,7 +11,7 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_login_view()
     {
         $response = $this->get('/login');
 
