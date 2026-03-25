2a3c1296ec1fff50905b614b7b23f97fc438279b
ArRahmaan17 <ardrah17@gmail.com>
2026-03-25 20:54:04 +0700

    feat: forgot password

--- app/Http/Controllers/PasswordResetController.php
+++ app/Http/Controllers/PasswordResetController.php
@@ -3,19 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Str;
-use Illuminate\Auth\Events\PasswordReset;
 
 class PasswordResetController extends Controller
 {
     public function showLinkRequestForm()
     {
         $title = 'Forgot Password';
+
         return view('auth.forgot-password', compact('title'));
     }
 
@@ -38,6 +37,7 @@ class PasswordResetController extends Controller
     public function showResetForm(Request $request, $token = null)
     {
         $title = 'Reset Password';
+
         return view('auth.reset-password', compact('title'))->with(
             ['token' => $token, 'email' => $request->email]
         );
@@ -58,7 +58,7 @@ class PasswordResetController extends Controller
             $request->only('email', 'password', 'password_confirmation', 'token'),
             function ($user, $password) {
                 $user->forceFill([
-                    'password' => Hash::make($password)
+                    'password' => Hash::make($password),
                 ])->setRememberToken(Str::random(60))->save();
 
                 event(new PasswordReset($user));
