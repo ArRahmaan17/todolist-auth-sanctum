eeab65e7732df387bf1ec53633d61ebcd4f07d99
ArRahmaan17 <ardrah17@gmail.com>
2026-03-25 21:51:09 +0700

    style: laravel pint formatting

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,14 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class HomeController extends Controller
 {
     public function index()
     {
         $app_name = env('APP_NAME', 'TodoList');
-        $title = $app_name . ' - Welcome';
+        $title = $app_name.' - Welcome';
+
         return view('home', compact('title', 'app_name'));
     }
 }
