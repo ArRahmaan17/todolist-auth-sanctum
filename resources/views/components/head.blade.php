ab3d6da2084a535951e15bd64f5c74b3c2eb04f3
RahmaanM17 <ardrah17@gmail.com>
2026-03-29 05:08:56 +0700

    update: head component

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -5,6 +5,7 @@
 <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}" />
 <meta name="apple-mobile-web-app-title" content="{{ $app_name ?? 'Todos' }}" />
 <link rel="manifest" href="{{ asset('site.webmanifest') }}" />
+
 <meta property="og:title" content="Todo - Secure To-Do App">
 <meta name=description content="Simple todo app built with Laravel with Sanctum authentication">
 <meta property="og:description" content="Simple todo app built with Laravel with Sanctum authentication">
